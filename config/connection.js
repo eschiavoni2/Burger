6ced899a3542cec7aa3901a82267981c4f00ea23
Erika Zibelnik <e.m.zibelnik@gmail.com>
2020-10-19 15:59:07 -0400

    adding new text, and moving burger of the day

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
-
-var connection = mysql.createConnection({
+// Global for connection
+var connection;
+// If on Heroku use the Envirnmental Variable for JawsDB
+if (process.env.JAWSDB_URL){
+  connection = mysq.creatConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "M1qu3tt314!",
   database: "burgers_db"
-});
+  });
+};
 
 // Make connection.
 connection.connect(function(err) {
